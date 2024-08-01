dc24caa23aca190e5021bc3f65623d779fc4d19e
Kunal Kumar <kumarkunal8759@gmail.com>
2024-08-01 19:12:30 +0530

    added changes

--- src/utils/ExecutorFactory.ts
+++ src/utils/ExecutorFactory.ts
@@ -5,15 +5,15 @@ import { ExecutionResponse } from "../types/codeExecutorStrategy";
 
 
 function createExecutor(codeLanguage : string,code:string,inputTestCase:string,outputTestCase:string):Promise<ExecutionResponse> | null{
-    if(codeLanguage ==="PYTHON"){
+    if(codeLanguage.toUpperCase() ==="PYTHON"){
         const pythonObject=new PythonExecutor();
         return pythonObject.execute(code,inputTestCase,outputTestCase);
     }
-    else if(codeLanguage==="CPP"){
+    else if(codeLanguage.toUpperCase()==="CPP"){
         const cppObject=new CppExecutor();
         return cppObject.execute(code,inputTestCase,outputTestCase);
     }
-    else if(codeLanguage==="JAVA"){
+    else if(codeLanguage.toUpperCase()==="JAVA"){
         const javaObject=new JavaExecutor();
         return javaObject.execute(code,inputTestCase,outputTestCase);
     }
